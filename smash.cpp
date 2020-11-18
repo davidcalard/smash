ab5bf734fda6b1d990d911539eb5a2cddcb091af
moradnir <49983799+moradnir@users.noreply.github.com>
2020-11-18 23:03:49 +0200

    Update smash.cpp

--- smash.cpp
+++ smash.cpp
@@ -18,7 +18,8 @@ int main(int argc, char* argv[]) {
     //TODO: setup sig alarm handler
 
     while(true) {
-        std::cout << smash.getPrompt(); // TODO: change this (why?)
+        //std::cout << smash.getPrompt(); // TODO: change this (why?)
+        smash.printPrompt();
         std::string cmd_line;
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
