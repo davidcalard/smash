8d8299051865e4e86151e57ced3395c02e859ec4
moradnir <49983799+moradnir@users.noreply.github.com>
2020-11-21 22:40:49 +0200

    Update signals.cpp

--- signals.cpp
+++ signals.cpp
@@ -4,16 +4,31 @@
 #include "Commands.h"
 
 using namespace std;
+extern SmallShell& smash;
 
 void ctrlZHandler(int sig_num) {
-	// TODO: Add your implementation
+    cout << "smash: got ctrl-Z" << endl;
+    if (smash.getCurrCmd() != nullptr){
+        smash.getJobsList().addJob(smash.getCurrCmd(), true);
+        if (kill(smash.getCurrCmd()->getMyPid(), SIGTSTP) < 0){
+            perror("smash error: kill failed");
+            return;
+        }
+        cout << "smash: process " << smash.getCurrCmd()->getMyPid() << " was stopped" << endl;
+    }
 }
 
 void ctrlCHandler(int sig_num) {
-  // TODO: Add your implementation
+    cout << "smash: got ctrl-C" << endl;
+    if (smash.getCurrCmd() != nullptr){
+        if (kill(smash.getCurrCmd()->getMyPid(), SIGKILL) < 0){
+            perror("smash error: kill failed");
+            return;
+        }
+        cout << "smash: process " << smash.getCurrCmd()->getMyPid() << " was killed" << endl;
+    }
 }
 
 void alarmHandler(int sig_num) {
-  // TODO: Add your implementation
+    // TODO: Add your implementation
 }
-
