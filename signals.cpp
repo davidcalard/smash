28e47f0701b60023568aa4cd8393aea19dbe76f0
moradnir <49983799+moradnir@users.noreply.github.com>
2020-11-25 17:50:21 +0200

    Update signals.cpp

--- signals.cpp
+++ signals.cpp
@@ -16,6 +16,10 @@ void ctrlZHandler(int sig_num) {
         }
         cout << "smash: process " << smash.getCurrCmd()->getMyPid() << " was stopped" << endl;
     }
+    else{
+        smash.printPrompt();
+        cout << flush;
+    }
 }
 
 void ctrlCHandler(int sig_num) {
@@ -27,8 +31,27 @@ void ctrlCHandler(int sig_num) {
         }
         cout << "smash: process " << smash.getCurrCmd()->getMyPid() << " was killed" << endl;
     }
+    else{
+        smash.printPrompt();
+        cout << flush;
+    }
 }
 
 void alarmHandler(int sig_num) {
-    // TODO: Add your implementation
+    cout << "smash got an alarm" << endl;
+    //string str(smash.getJobsList().job_list.rbegin()->second.cmd->getCmd());
+    //bool bg_flag = str[str.find_last_not_of(" ")] == '&';
+    for (auto iterator = smash.getJobsList().job_list.begin(); iterator != smash.getJobsList().job_list.end(); ++iterator) {
+        time_t curr_time;
+        time(&curr_time);
+        if (iterator->second.cmd->getAlarmTime() <= difftime(curr_time,iterator.operator*().second.start_time)){
+            cout << "smash: " << iterator->second.cmd->getCmd() << " timed out!" << endl;
+            if(kill(iterator->second.cmd->getMyPid(), SIGKILL) < 0){
+                perror("smash error: kill failed");
+                return;
+            }
+        }
+    }
+    //smash.printPrompt();
+    //cout << flush;
 }
