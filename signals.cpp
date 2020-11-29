dae944fee430f4cc8b370e016ba8a763c74e6daa
moradnir <49983799+moradnir@users.noreply.github.com>
2020-11-29 02:33:28 +0200

    Update signals.cpp

--- signals.cpp
+++ signals.cpp
@@ -16,10 +16,10 @@ void ctrlZHandler(int sig_num) {
         }
         cout << "smash: process " << smash.getCurrCmd()->getMyPid() << " was stopped" << endl;
     }
-    else{
+    /*else{
         smash.printPrompt();
         cout << flush;
-    }
+    }*/
 }
 
 void ctrlCHandler(int sig_num) {
@@ -31,26 +31,43 @@ void ctrlCHandler(int sig_num) {
         }
         cout << "smash: process " << smash.getCurrCmd()->getMyPid() << " was killed" << endl;
     }
-    else{
+    /*else{
         smash.printPrompt();
         cout << flush;
-    }
+    }*/
 }
 
 void alarmHandler(int sig_num) {
     cout << "smash got an alarm" << endl;
+    time_t curr_time;
+    time(&curr_time);
     //string str(smash.getJobsList().job_list.rbegin()->second.cmd->getCmd());
     //bool bg_flag = str[str.find_last_not_of(" ")] == '&';
-    for (auto iterator = smash.getJobsList().job_list.begin(); iterator != smash.getJobsList().job_list.end(); ++iterator) {
-        time_t curr_time;
-        time(&curr_time);
-        if (iterator->second.cmd->getAlarmTime() <= difftime(curr_time,iterator.operator*().second.start_time)){
-            cout << "smash: " << iterator->second.cmd->getCmd() << " timed out!" << endl;
-            if(kill(iterator->second.cmd->getMyPid(), SIGKILL) < 0){
+    for (auto iterator = smash.alarm_jobs.begin(); iterator != smash.alarm_jobs.end(); ) {
+        if (iterator.operator*()->getAlarmTime() <= difftime(curr_time,iterator.operator*()->getInitTime())){
+            cout << "smash: " << iterator.operator*()->getCmd() << " timed out!" << endl;
+            if(kill(iterator.operator*()->getMyPid(), SIGKILL) < 0){
                 perror("smash error: kill failed");
                 return;
             }
+            auto temp = iterator;
+            ++iterator;
+            smash.alarm_jobs.remove(temp.operator*());
+            continue;
         }
+        ++iterator;
+    }
+    time_t next_alarm_time = smash.alarm_jobs.begin() == smash.alarm_jobs.end() ? NOT_ALARM :
+            (smash.alarm_jobs.begin().operator*()->getAlarmTime() - difftime(curr_time, smash.alarm_jobs.begin().operator*()->getInitTime()));
+    if(next_alarm_time != NOT_ALARM) {
+        for (auto iterator = smash.alarm_jobs.begin(); iterator != smash.alarm_jobs.end(); ++iterator) {
+            time_t temp = (iterator.operator*()->getAlarmTime() -
+                           difftime(curr_time, iterator.operator*()->getInitTime()));
+            if (temp < next_alarm_time) {
+                next_alarm_time = temp;
+            }
+        }
+        alarm(next_alarm_time); //Always success
     }
     //smash.printPrompt();
     //cout << flush;
